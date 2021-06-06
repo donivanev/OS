02842349301c25448eb9350dcfada67c62ae7950
Doni <dok246@gmail.com>
2021-06-06 18:30:46 +0300

    Create problem13.6.c

---
+++ C/problem13.6.c
@@ -0,0 +1,51 @@
+// • Да се напише програма на C, която получава като параметри в командния ред 2 команди (без параметри). 
+//   Програмата реализира конструкцията: команда1 || команда2
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wait.h>
+
+int main(int argc, char* argv[])
+{
+	int status;
+	int pid = fork();
+
+	if(pid == -1)
+	{
+		write(1, "Error while forking.\n", 22);
+		exit(-1);
+	}
+	else if(pid > 0)
+	{
+		pid_t childPid = wait(&status);
+
+		if (status != 0)
+		{
+			if(execlp(argv[2], argv[2], 0) != -1)
+			{
+				execlp(argv[2], argv[2], 0);
+			}
+			else
+			{
+				write(1, "Command 2 not found.\n", 22);
+				exit(-1);
+			}
+		}
+	}
+	else
+	{
+		if(execlp(argv[1], argv[1], 0) != -1)
+		{
+			execlp(argv[1], argv[1], 0);
+		}
+		else
+		{
+			write(1, "Command 1 not found.\n", 22);
+			exit(-1);
+		}
+	}
+
+	exit(0);
+}
